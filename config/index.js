605bdf0dbf04535f2b1b6bcad87575cdc34b94b0
huangtingu <iing7050737@gmail.com>
2020-04-04 06:25:58 +0800

    在config/index.js 調整設定檔的路徑

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: './clothing-shop/',
+    assetsPublicPath: './',
     proxyTable: {},
 
     // Various Dev Server settings
